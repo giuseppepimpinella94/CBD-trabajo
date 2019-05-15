57db5cfa203611f39ddbe107de06989324c259f6
sos1819gp <pimpinella.giuseppe@libero.it>
2019-05-15 15:20:52 +0000

    progetto

--- public/main-ctrl.js
+++ public/main-ctrl.js
@@ -5,50 +5,6 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http){
      console.log("Modular MainCtrl climate initialized!");   
      $scope.url="/api/v1/climate-stats";
      
-     function refresh()
-    {
-        console.log("Requesting API");
-        var date = document.getElementById("date").value;
-        var URL = $scope.url + "?FECHA=" + date;
-        $http.get(URL).then(function(response){
-            console.log("Data received " + JSON.stringify(response.data, null, 2));
-            $scope.climates = response.data;
-        }, 
-        function (error){});
-    }
-    
-    function initialize() 
-    { 
-        document.getElementById("divpagina").style.visibility = "hidden"; 
-        document.getElementById("btnprvpg").style.visibility = "hidden"; 
-        document.getElementById("btnsuccpg").style.visibility = "hidden"; 
-    }
-
-    initialize();
-
-    $scope.getPagination = function (pag){ 
-        if (pag == 1) 
-        {
-            document.getElementById("divpagina").style.visibility = "visible";
-            document.getElementById("btnprvpg").style.visibility = "hidden"; 
-            document.getElementById("btnsuccpg").style.visibility = "visible";
-        }
-        else document.getElementById("btnprvpg").style.visibility = "visible";
-        $scope.pagina = pag;
-        var URL = $scope.url + "?offset=" + ($scope.pagina-1)*10 + "&limit=" + (($scope.pagina-1)*10 + 10);
-        $http.get(URL).then(function(response){
-            console.log("Data received " + JSON.stringify(response.data, null, 2));
-            document.getElementById("btnsuccpg").style.visibility = "visible";
-            $scope.climates = response.data;
-        }, 
-        function (error){});
-        $http.get($scope.url + "?offset=" + ($scope.pagina)*3 + "&limit=" + ($scope.pagina)*3 + 3).then(function(response){}, 
-        function (error){
-            document.getElementById("btnsuccpg").style.visibility = "hidden";
-        });
-        $scope.information = "Extraido los campos de Pagina " + $scope.pagina; 
-    };
-    
     
     $scope.finddate = function (){
         var date = document.getElementById("date").value;
